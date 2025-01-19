7ab5c87d6d51001f0167ced36860a6fd42e48b2e
LCoffe <leonardo.021103@alunos.utfpr.edu.br>
2025-01-19 17:53:05 -0300

    removendo textura

--- Jogo/src/Entidade.cpp
+++ Jogo/src/Entidade.cpp
@@ -6,31 +6,13 @@ constexpr auto PATH_INIMIGO = ".\\assets\\athenasoldier.png";
 namespace Entidade {
 	Entidade::Entidade(sf::Vector2f pos, sf::Vector2f tam, const IDs::IDs ID) : Ente(ID), pos(pos), tam(tam),sprite(),
 	remover(false){
-		
-		
-		/*
-		
-		if (ID == IDs::IDs::jogador) {
-			corpo.setTexture(pGG->incluirTextura(PATH_JOGADOR));
-		}
-		*/
-		if (ID == IDs::IDs::inimigo) {
-			corpo.setTexture(pGG->incluirTextura(PATH_INIMIGO));
-		}
-		else {
-			corpo.setFillColor(sf::Color::White);
-		}
-		
-		
-		
 		this->setTam(tam);
 		this->setPos(pos);
-		
 	}
 
 	Entidade::~Entidade() {}
 
 	void Entidade::desenhar() {
-		pGG->desenharElemento(corpo);
+		//pGG->desenharElemento(corpo);
 	}
 }
